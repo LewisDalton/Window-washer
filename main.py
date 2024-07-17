db82bc9de1facaf0ff0dcd83c3b832a51af84e6a
LewisDalton <139710354+LewisDalton@users.noreply.github.com>
2024-07-17 14:59:18 +0100

    working animation loop for player while moving

--- main.py
+++ main.py
@@ -25,12 +25,6 @@ class Game:
         
     def run(self):
         while True:
-            # fill the screen with a color to wipe away anything from last frame
-            self.screen.fill((84, 192, 214))
-           
-            self.player.update_pos()
-            self.player.render(self.screen)
-
             # poll for events
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -49,6 +43,12 @@ class Game:
                         self.player.movement_x[1] = False
                     if event.key == pygame.K_a:
                         self.player.movement_x[0] = False
+            
+            # Drawing
+            self.screen.fill((84, 192, 214))
+            self.player.update_pos()
+            self.player.render(self.screen)
+            self.player.update_anim()
 
             # flip() the display to put your work on screen
             pygame.display.flip()
--- player.py
+++ player.py
@@ -6,27 +6,34 @@ import os
 class Player:
     def __init__(self, res_x, res_y) -> None:
         super().__init__()
-        self.sprite = pygame.image.load(os.path.join('assets', '.png_files' ,'idle_character.png'))
         self.sprite_size = [160, 160]
-        self.sprite = pygame.transform.scale(self.sprite, self.sprite_size)
         self.player_pos = [0, 0]
         self.movement_y = False
         self.movement_x = [False, False]
 
-
-        '''self.velocity = 10
-        self.pos_x = 0
-        self.pos_y = 0
         self.sprites: list = []
         self.sprites.append(pygame.image.load(os.path.join('assets', '.png_files' ,'idle_character.png')))
         self.sprites.append(pygame.image.load(os.path.join('assets', '.png_files' ,'moving_up_left_hand.png')))
         self.sprites.append(pygame.image.load(os.path.join('assets', '.png_files' ,'moving_up_right_hand.png')))
         self.current_sprite: int = 0
-        self.image = self.sprites[self.current_sprite]'''
+        self.image = self.sprites[self.current_sprite]
+        self.image = pygame.transform.scale(self.image, self.sprite_size)
+        self.rect = self.image.get_rect()
+        self.rect.topleft = self.player_pos
 
     def update_pos(self):
         self.player_pos[0] += self.movement_x[1] * 5
         self.player_pos[0] -= self.movement_x[0] * 5
  
     def render(self, screen):
-        screen.blit(self.sprite, self.player_pos)
+        screen.blit(self.image, self.player_pos)
+
+    def update_anim(self):
+        if self.movement_x[0] == True or self.movement_x[1] == True:
+            self.current_sprite += 0.1
+
+            if self.current_sprite >= len(self.sprites):
+                self.current_sprite = 0
+
+            self.image = self.sprites[int(self.current_sprite)]
+            self.image = pygame.transform.scale(self.image, self.sprite_size)
