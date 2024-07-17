23552e85c8e2ac1ded46b21247466d8326faba1b
LewisDalton <139710354+LewisDalton@users.noreply.github.com>
2024-07-17 14:25:46 +0100

    working seperate scripts for classes and methods

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 import os
 import math
 import sys
+from player import Player
 
 # pygame setup
 
@@ -10,24 +11,25 @@ class Game:
         pygame.init()
 
         pygame.display.set_caption('Window Washer')
-        self.screen = pygame.display.set_mode((828, 1040))
+
+        self.res_x = 824
+        self.res_y = 1020
+        self.screen = pygame.display.set_mode((self.res_x, self.res_y))
 
         self.clock = pygame.time.Clock()
-
         self.dt = 0
 
-        self.sprite = pygame.image.load(os.path.join('assets', '.png_files' ,'idle_character.png'))
-        self.sprite = pygame.transform.scale(self.sprite, (160, 160))
-        self.player_pos = [0, 0]
-        self.velocity = 10
-
-
-
+        self.player = Player(self.res_x, self.res_y)
+        self.player.player_pos = [(self.res_x / 2) - (self.player.sprite_size[0] / 2), 
+                                  (self.res_y / 3 * 2) - (self.player.sprite_size[1] / 2)]
+        
     def run(self):
         while True:
-
-            move_x = 0
-            move_y = 0
+            # fill the screen with a color to wipe away anything from last frame
+            self.screen.fill((84, 192, 214))
+           
+            self.player.update_pos()
+            self.player.render(self.screen)
 
             # poll for events
             for event in pygame.event.get():
@@ -38,40 +40,15 @@ class Game:
                     if event.key == pygame.K_ESCAPE:
                         pygame.quit()
                         sys.quit()
-
-                keys = pygame.key.get_pressed()
-
-                if keys[pygame.K_w]:
-                    move_y -= 1
-                if keys[pygame.K_s]:
-                    move_y += 1
-                if keys[pygame.K_a]:
-                    move_x -= 1 
-                if keys[pygame.K_d]:
-                    move_x += 1 
-        
-            # Normalizing the movement for diagonals
-            if move_y != 0 and move_x != 0:
-                move_y = move_y / math.sqrt(2)
-                move_x = move_x / math.sqrt(2)
-
-            '''while move_x != 0 or move_y != 0:
-            current_sprite = player_up_left
-            
-            current_sprite = player_up_right
-            else:
-                current_sprite = player_idle'''
-
-            # Applying the movement to the co-ordinates
-            self.player_pos[0] += move_x * self.velocity
-            self.player_pos[1] += move_y * self.velocity
-
-
-            # fill the screen with a color to wipe away anything from last frame
-            self.screen.fill((84, 192, 214))
-
-            # Drawing character images
-            self.screen.blit(self.sprite, self.player_pos)
+                    if event.key == pygame.K_d:
+                        self.player.movement_x[1] = True
+                    if event.key == pygame.K_a:
+                        self.player.movement_x[0] = True
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_d:
+                        self.player.movement_x[1] = False
+                    if event.key == pygame.K_a:
+                        self.player.movement_x[0] = False
 
             # flip() the display to put your work on screen
             pygame.display.flip()
--- player.py
+++ player.py
@@ -4,9 +4,17 @@ import os
 
 #  Sprites
 class Player:
-    def __init__(self) -> None:
+    def __init__(self, res_x, res_y) -> None:
         super().__init__()
-        self.velocity = 10
+        self.sprite = pygame.image.load(os.path.join('assets', '.png_files' ,'idle_character.png'))
+        self.sprite_size = [160, 160]
+        self.sprite = pygame.transform.scale(self.sprite, self.sprite_size)
+        self.player_pos = [0, 0]
+        self.movement_y = False
+        self.movement_x = [False, False]
+
+
+        '''self.velocity = 10
         self.pos_x = 0
         self.pos_y = 0
         self.sprites: list = []
@@ -14,39 +22,11 @@ class Player:
         self.sprites.append(pygame.image.load(os.path.join('assets', '.png_files' ,'moving_up_left_hand.png')))
         self.sprites.append(pygame.image.load(os.path.join('assets', '.png_files' ,'moving_up_right_hand.png')))
         self.current_sprite: int = 0
-        self.image = self.sprites[self.current_sprite]
-        pass
+        self.image = self.sprites[self.current_sprite]'''
 
-    def controls(self):
-
-    keys = pygame.key.get_pressed()
-
-    move_x = 0
-    move_y = 0
-
-    if keys[pygame.K_w]:
-        move_y -= 1
-    if keys[pygame.K_s]:
-        move_y += 1
-    if keys[pygame.K_a]:
-        move_x -= 1 
-    if keys[pygame.K_d]:
-        move_x += 1 
-   
-    # Normalizing the movement for diagonals
-    if move_y != 0 and move_x != 0:
-        move_y = move_y / math.sqrt(2)
-        move_x = move_x / math.sqrt(2)
-
-    '''while move_x != 0 or move_y != 0:
-       current_sprite = player_up_left
-       
-       current_sprite = player_up_right
-    else:
-        current_sprite = player_idle'''
-
-    # Applying the movement to the co-ordinates
-    self.pos_x += move_x * self.velocity
-    self.pos_y += move_y * self.velocity
+    def update_pos(self):
+        self.player_pos[0] += self.movement_x[1] * 5
+        self.player_pos[0] -= self.movement_x[0] * 5
  
- 
+    def render(self, screen):
+        screen.blit(self.sprite, self.player_pos)
